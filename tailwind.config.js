cb7490c4d17c5bc5c7453e78ec9620c306abd572
maratsumi <dextroopsgaming@gmail.com>
2022-11-28 12:56:38 +0800

    initial dashboard made; script added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./*.{html,js}", "./components/*.{html,js}"],
   theme: {
+    fontFamily: {
+      display: ['"Open Sans"'],
+      body: ['"Open Sans"'],
+    },
     extend: {},
   },
   plugins: [],
