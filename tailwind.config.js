769186567eff5c0e7a4d0620e9d3f44952c65027
maratsumi <dextroopsgaming@gmail.com>
2022-11-28 10:42:56 +0800

    tailwnid test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.{html,js}", "./components/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
