db74aa6d7042ca71656680ae712182e673ffa56b
Kimotee327 <joaquimbernardo94@gmail.com>
2022-12-01 16:12:29 +0800

    Update config.php

--- components/config.php
+++ components/config.php
@@ -5,7 +5,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_NAME', 'sml');
  
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
