48a56bc0de942210bed87581b35f315a7d72eb4b
maratsumi <dextroopsgaming@gmail.com>
2022-12-02 11:51:33 +0800

    removed unnecessary comment

--- createLog.php
+++ createLog.php
@@ -15,9 +15,6 @@
     } elseif(!filter_var($input_dateOrdered, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^(((\d{4})(-)(0[13578]|10|12)(-)(0[1-9]|[12][0-9]|3[01]))|((\d{4})(-)(0[469]|1‌​1)(-)([0][1-9]|[12][0-9]|30))|((\d{4})(-)(02)(-)(0[1-9]|1[0-9]|2[0-8]))|(([02468]‌​[048]00)(-)(02)(-)(29))|(([13579][26]00)(-)(02)(-)(29))|(([0-9][0-9][0][48])(-)(0‌​2)(-)(29))|(([0-9][0-9][2468][048])(-)(02)(-)(29))|(([0-9][0-9][13579][26])(-)(02‌​)(-)(29)))(\s([0-1][0-9]|2[0-4]):([0-5][0-9]):([0-5][0-9]))$/")))){
       $dateOrdered_err = "Please enter a valid Date";
     } else {
-      // $dateOrderedString = $input_dateOrdered;
-      // $dateOrdered = strtotime($dateOrderedString);
-      // date("Y-m-d H:i:s", $dateOrdered);
       $time = strtotime($input_dateOrdered);
       $dateOrdered = date('Y-m-d H:i:s', $time);
     }
